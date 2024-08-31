be20a0f50e7054f4eb48c4510044f76fe8e625d8
aldio <muhamadaldioyaspindo@gmail.com>
2024-08-31 11:14:38 +0700

    editing contact.php

--- contact.php
+++ contact.php
@@ -1,12 +1,35 @@
+<link rel="stylesheet" href="style.css">
+<div class="text">
+<h1>The Design<br> Web From <br>Tecno House</h1>
+<p>Nantikan Konten dan Project <br>Selanjutnya hanya di <br>Tecno House.com</p>
+</div>
+
+<div class="body">
+
 <form action="process_contact.php" method="post">
-    <label for="name">Your name:</label>
-    <input type="text" id="name" name="name" required>
+    <div class="name">
+    <label for="name" class="lbl-name">
+        <span class="content-name">Username</span>
+    </label>
+    <input type="text" id="name" name="name" required placeholder="Your Name">
+    </div>
 
-    <label for="email">Your Email:</label>
-    <input type="email" id="email" name="email" required>
+    <div class="email">
+    <label for="email">
+        <span class="content-email">Email</span>
+    </label>
+    <input type="email" id="email" name="email" required placeholder="Email@gmail.com">
+    </div>
 
-    <label for="message">Your Message</label>
-    <textarea name="message" id="message" required></textarea>
+    <div class="message">
+    <label for="message">
+        <span class="content-message">Message</span>
+    </label>
+    <textarea name="message" id="message" required placeholder="Tell us What You Need"></textarea>
+    </div>
 
-    <button type="submit">Submit</button>
+    <div class="submit">
+    <a href="#" class="btn">Submit</a>
+    </div>
 </form>
+</div>
